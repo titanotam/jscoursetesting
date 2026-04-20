e9600c23b32e55afa6b070b22a315a69ba3590ab
huynhnhuttam <huynhnhuttam@gmail.com>
2026-04-20 12:00:31 -0400

    message

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,13 @@ function calculateArea() {
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+
+let groc1, groc2, groc3;
+function groceryTracker() {
+    groc1 = parseFloat(document.getElementById('grocery1').value);
+    groc2 = parseFloat(document.getElementById('grocery2').value);
+    groc3 = parseFloat(document.getElementById('grocery3').value);
+    let result = groc1 + groc2 + groc3;
+    document.getElementById('grocery-result').innerText = `The total amount is: ${result}`;
+}
